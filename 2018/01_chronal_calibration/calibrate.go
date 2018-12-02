04cea3d32b804fddf97debfdf997310889e25c76
Rob Jackson <rob@rjackson.me>
2018-12-02 11:29:13 +0000

    Solve day 1, part 2

--- 2018/01_chronal_calibration/calibrate.go
+++ 2018/01_chronal_calibration/calibrate.go
@@ -1,12 +1,19 @@
 package main
 
-// Calibrate the chronal device with the
+// Calibrate the chronal device with the list of possible corrections
+// The correct frequency is the one we observe multiple times
 func Calibrate(freqChanges []int) int {
 	var frequency int
+	var seenFreqs = make(map[int]bool, len(freqChanges))
 
-	for _, correction := range freqChanges {
-		frequency += correction
+	for {
+		for _, correction := range freqChanges {
+			if seenFreqs[frequency] {
+				return frequency
+			}
+			seenFreqs[frequency] = true
+
+			frequency += correction
+		}
 	}
-
-	return frequency
 }
--- 2018/01_chronal_calibration/calibrate_test.go
+++ 2018/01_chronal_calibration/calibrate_test.go
@@ -9,20 +9,20 @@ var testCases = []struct {
 	expected int
 }{
 	{
-		input:    []int{1, -2, 3, 1},
-		expected: 3,
-	},
-	{
-		input:    []int{1, 1, 1},
-		expected: 3,
-	},
-	{
-		input:    []int{1, 1, -2},
+		input:    []int{1, -1},
 		expected: 0,
 	},
 	{
-		input:    []int{-1, -2, -3},
-		expected: -6,
+		input:    []int{3, 3, 4, -2, -4},
+		expected: 10,
+	},
+	{
+		input:    []int{-6, 3, 8, 5, -6},
+		expected: 5,
+	},
+	{
+		input:    []int{7, 7, -2, -7, -4},
+		expected: 14,
 	},
 }
 
